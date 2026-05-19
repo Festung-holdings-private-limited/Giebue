f71a4299d359b3e41e56b19acb63cf6698289ffc
Festung Holdings <contact@giebue.com>
2026-05-20 01:03:49 +0530

    final stable build

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/Giebue",
-  assetPrefix: "/Giebue/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
